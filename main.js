f9894c3bd1707f808c92753982809618350a640f
ykb136 <13806571666@163.com>
2026-02-26 22:28:43 +0800

    完成了首页的开发

--- main.js
+++ main.js
@@ -2,6 +2,30 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+//导入网络请求包
+// import {$http} from '@escook/request-miniprogram'
+// uni.$http=$http
+// $http.baseUrl='https://api-hmugo-web.itheima.net'
+//请求拦截器
+// $http.beforeRequest=function(options){
+//     uni.showLoading({
+//         title:'数据加载中...'
+//     })
+// }
+//  //响应拦截器
+// $http.afterRequest=function(){
+//     uni.hideLoading()
+// }
+
+//封装弹框的方法
+    
+uni.$showMsg =function(title='数据请求失败!' , duration=1500){
+    uni.showToast({
+        title,
+        duration,
+        icon:'none'
+    })
+}
 
 Vue.config.productionTip = false
 
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,13 +1,127 @@
 <template>
     <view>
-        Home
+      <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+          <swiper-item v-for="(item,i) in swiperList" :key="i">
+              <navigator class="swiper-item" :url="`/subpkg/goods_detail/goods_detail?goods_id=${item.goods_id}`">
+                  <image :src="item.image_src"></image>
+              </navigator>
+          </swiper-item>
+          
+      </swiper>
+      <!-- //分类区 -->
+      <view class="nav-list">
+          <view class="nav-item" v-for="(item,i) in navList" :key="i" @click="navClickHandler(item)">
+              <image :src="item.image_src" class="nav-img"></image>    
+          </view>
+      </view>
     </view>
+    
+   <view class="floor-list">
+       <view class="floor-item" v-for="(item,i) in floorList" :key="i">
+           <image :src="item.floor_title.image_src" class="floor-title"></image>
+           <view class="floor-img-box">
+               <navigator class="left-img-box" :url="item.product_list[0].url">
+                  <image :src="item.product_list[0].image_src" :style="{width:item.product_list[0].image_width+ 'rpx'}" mode="widthFix"></image>
+               </navigator>
+               <view class="right-img-box">
+                   <navigator class="right-img-item" v-for="(item2,i2) in item.product_list.slice(1)" :key="i2" :url="item2.url">
+                       <image :src="item2.image_src" mode="widthFix" :style="{width: item2.image_width+'rpx'}"></image>
+                   </navigator>
+               </view>
+           </view>
+       </view>
+   </view> 
+    
 </template>
 
-<script setup>
-    
+<script >
+   export default {
+        data() {
+            return {
+                //这是轮播图的数据列表
+                swiperList:[],
+                navList:[],
+                floorList:[]
+            };
+        },
+        onLoad(){
+            this.getswiperList();
+            this.getNavList();
+            this.getFloorList();
+        },
+        methods :{
+            
+            //uni.$http.get 改为 uni.request({url:'/api/public/v1/home/swiperdata ', method:' GET'})也可以获取到数据并且不会报错
+          async  getswiperList(){
+          // const res=   await  uni.$http.get('/api/public/v1/home/swiperdata')
+          const {data:res}=   await uni.request({url:'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata', method:'GET'})
+              console.log(res);
+              if(res.meta.status !==200){
+                  return uni.$showMsg()
+              }
+              this.swiperList=res.message
+              uni.$showMsg('数据请求成功')
+              console.log(this.swiperList)
+            },
+         async   getNavList(){
+             const {data:res} =await  uni.request({ url:'https://api-hmugo-web.itheima.net/api/public/v1/home/catitems', method:'GET' })
+             if (res.meta.status !==200) return uni.$showMsg()
+             this.navList=res.message
+            },
+                
+            navClickHandler(item){
+                if(item.name==='分类'){
+                    uni.switchTab({
+                        url:'/pages/cate/cate'
+                    })
+                }
+            },
+          async  getFloorList(){
+                const {data:res} =await  uni.request({ url:'https://api-hmugo-web.itheima.net/api/public/v1/home/floordata', method:'GET' })
+                if (res.meta.status !==200) return uni.$showMsg()
+                console.log(res)
+                res.message.forEach(floor =>{
+                    floor.product_list.forEach(prod=>{
+                        prod.url='/subpkg/goods_list/goods_list?'+prod.navigator_url.split('?')[1]
+                    })
+                })
+                this.floorList=res.message
+            },
+        }
+    };
 </script>
 
 <style lang="scss">
-
+    swiper{
+        height: 330rpx;
+        .swiper-item,
+        image{
+            width: 100%;
+            height: 100%;
+        }
+    }
+        
+    .nav-list{
+        display: flex;
+        justify-content: space-around;
+        margin: 15px 0;
+    }
+        
+    .nav-img{
+        width: 128rpx;
+        height: 140rpx;
+    }
+    .floor-title{
+        width: 100%;
+        height: 60rpx;
+    }
+    .right-img-box{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+    }
+    .floor-img-box{
+        display: flex;
+        padding-left: 10rpx;
+    }
 </style>
